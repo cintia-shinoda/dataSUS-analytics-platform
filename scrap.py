b61f10f30714ecf90e2c4cac6d2b23a72a8b21a7
Natan Camargos <natanscamargos@gmail.com>
2022-03-17 00:49:55 -0300

    Fetch das páginas com recorrência

--- scrap.py
+++ scrap.py
@@ -1,32 +1,46 @@
+from time import sleep
 import requests
 from requests.auth import HTTPBasicAuth
 import pandas as pd
 import json
 
-url = "https://imunizacao-es.saude.gov.br/_search"
+url1 = "https://imunizacao-es.saude.gov.br/_search?scroll=1m"
+url2 = "https://imunizacao-es.saude.gov.br/_search/scroll"
 
-payload = json.dumps({
-  "size": 10000
-})
 headers = {
   'Authorization': 'Basic aW11bml6YWNhb19wdWJsaWM6cWx0bzV0JjdyX0ArI1Rsc3RpZ2k=',
   'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+# df_vacina = pd.json_normalize(vacina['hits']['hits'])
 
-# status da requisição
-print(response)
+def paginate(scroll_id, page_number):
+  try:
+    print('Fetching page %s' % (page_number))
+    vacina = None
 
-# json para DF
-vacina = response.json()
-vacina['hits']['hits']
-df_vacina = pd.json_normalize(vacina['hits']['hits'])
-print(df_vacina)
+    if page_number == 0:
+      payload = json.dumps({
+        "size": 10000
+      })
+      vacina = requests.request("POST", url1, headers=headers, data=payload).json()
+    else:
+      payload = json.dumps({
+        "scroll": "1m",
+        "scroll_id": scroll_id
+      })
+      vacina = requests.request("POST", url2, headers=headers, data=payload).json()
 
-# Página 1
-# POST https://imunizacao-es.saude.gov.br/_search?scroll=1m
-# Body: { "size": 10000 }
-# Página 2,3,4...
-# POST https://imunizacao-es.saude.gov.br/_search/scroll
-# Body: { "scroll": "1m", "scroll_id": "scroll id da página anterior" }
+    next_scroll_id = vacina['_scroll_id']
+    hits = vacina['hits']['hits']
+    if len(hits) != 0:
+      sleep(3)
+      paginate(next_scroll_id, page_number + 1)
+    else:
+      print('All pages fetched')
+  except:
+    print("An occurred, trying again in 3s")
+    sleep(3)
+    paginate(scroll_id, page_number)
+
+paginate(None, 0)
