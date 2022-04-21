5a2466f923db09493179861054c85d1c9061cf46
Cintia Shinoda <shinoda.c.i@gmail.com>
2022-04-21 20:59:36 -0300

    1

--- script-for.py
+++ script-for.py
@@ -1,5 +1,52 @@
-i = 1
+import time
+import requests
+from requests.auth import HTTPBasicAuth
+import pandas as pd
+import json
+from time import sleep
 
-for i in range (i,11):
-    print("Contador do Baby Pigqueno" , i)
-    i+=1
+
+url1 = "https://imunizacao-es.saude.gov.br/_search?scroll=1m"
+url2 = "https://imunizacao-es.saude.gov.br/_search/scroll"
+
+headers = {
+    'Authorization': 'Basic aW11bml6YWNhb19wdWJsaWM6cWx0bzV0JjdyX0ArI1Rsc3RpZ2k=',
+    'Content-Type': 'application/json'
+}
+df_vacina = pd.DataFrame()
+
+
+page_number = 0
+scroll_id = None
+
+while True:
+  try:
+    print('Fetching page %s' % (page_number))
+    vacina = None
+
+    if page_number == 0:
+      payload = json.dumps({
+        "size": 10000
+      })
+      vacina = requests.request("POST", url1, headers=headers, data=payload).json()
+    else:
+      payload = json.dumps({
+        "scroll": "30m",
+        "scroll_id": scroll_id
+      })
+      vacina = requests.request("POST", url2, headers=headers, data=payload).json()
+
+    scroll_id = vacina['_scroll_id']
+    hits = vacina['hits']['hits']
+    df_vacina.update([pd.json_normalize(hits)])
+    if len(hits) != 0:
+      sleep(3)
+      page_number += 1
+      # paginate(next_scroll_id, page_number + 1)
+    else:
+      print('All pages fetched')
+      break
+  except:
+    print("An error occurred, trying again in 3s")
+    sleep(3)
+    # paginate(scroll_id, page_number)
