f8892de0136d2ceb952d657b5920d0dcd9749f97
Cintia Shinoda <shinoda.c.i@gmail.com>
2022-04-22 17:35:39 -0300

    v0.1

--- scrap.py
+++ scrap.py
@@ -4,51 +4,54 @@ from requests.auth import HTTPBasicAuth
 import pandas as pd
 import json
 from time import sleep
+import traceback
 
 
 url1 = "https://imunizacao-es.saude.gov.br/_search?scroll=1m"
 url2 = "https://imunizacao-es.saude.gov.br/_search/scroll"
 
 headers = {
-  'Authorization': 'Basic aW11bml6YWNhb19wdWJsaWM6cWx0bzV0JjdyX0ArI1Rsc3RpZ2k=',
-  'Content-Type': 'application/json'
+    'Authorization': 'Basic aW11bml6YWNhb19wdWJsaWM6cWx0bzV0JjdyX0ArI1Rsc3RpZ2k=',
+    'Content-Type': 'application/json'
 }
-
 df_vacina = pd.DataFrame()
 
-start = time.time()
 
-def paginate(scroll_id, page_number):
+page_number = 0
+scroll_id = None
+
+while True:
   try:
     print('Fetching page %s' % (page_number))
     vacina = None
 
     if page_number == 0:
       payload = json.dumps({
-        "size": 10000
+          "size": 10000
       })
-      vacina = requests.request("POST", url1, headers=headers, data=payload).json()
+      vacina = requests.request(
+          "POST", url1, headers=headers, data=payload).json()
     else:
       payload = json.dumps({
-        "scroll": "30m",
-        "scroll_id": scroll_id
+          "scroll": "30m",
+          "scroll_id": scroll_id
       })
-      vacina = requests.request("POST", url2, headers=headers, data=payload).json()
+      vacina = requests.request(
+          "POST", url2, headers=headers, data=payload).json()
 
-    next_scroll_id = vacina['_scroll_id']
+    scroll_id = vacina['_scroll_id']
     hits = vacina['hits']['hits']
-    pd.concat([df_vacina, pd.json_normalize(hits)])
+    df_vacina = pd.concat([df_vacina, pd.json_normalize(hits)])
     if len(hits) != 0:
       sleep(3)
-      paginate(next_scroll_id, page_number + 1)
+      page_number += 1
+      for index, row in df_vacina.iterrows():
+        uf = row['_source.estabelecimento_uf']
+        df_vacina.loc[[index]].to_csv(f'datasets/{uf}.csv', mode='a', header=False)
     else:
       print('All pages fetched')
-  except:
+      break
+  except Exception:
+    traceback.print_exc()
     print("An error occurred, trying again in 3s")
     sleep(3)
-    paginate(scroll_id, page_number)
-
-end = time.time()
-print((end - start)/60, "minutes")
-
-paginate(None, 0)
