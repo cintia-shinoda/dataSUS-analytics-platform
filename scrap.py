e78f830fb3df97d8e8c89be867f5f3c49243a1bb
Cintia Shinoda <shinoda.c.i@gmail.com>
2022-04-23 08:42:28 -0300

    adding timer

--- scrap.py
+++ scrap.py
@@ -6,7 +6,6 @@ import json
 from time import sleep
 import traceback
 
-
 url1 = "https://imunizacao-es.saude.gov.br/_search?scroll=1m"
 url2 = "https://imunizacao-es.saude.gov.br/_search/scroll"
 
@@ -14,20 +13,22 @@ headers = {
     'Authorization': 'Basic aW11bml6YWNhb19wdWJsaWM6cWx0bzV0JjdyX0ArI1Rsc3RpZ2k=',
     'Content-Type': 'application/json'
 }
+
 df_vacina = pd.DataFrame()
 
+chunk_size = 10000
 
 page_number = 0
 scroll_id = None
 
 while True:
+  start = time.time()
   try:
     print('Fetching page %s' % (page_number))
     vacina = None
-
     if page_number == 0:
       payload = json.dumps({
-          "size": 10000
+          "size": chunk_size
       })
       vacina = requests.request(
           "POST", url1, headers=headers, data=payload).json()
@@ -41,17 +42,20 @@ while True:
 
     scroll_id = vacina['_scroll_id']
     hits = vacina['hits']['hits']
-    df_vacina = pd.concat([df_vacina, pd.json_normalize(hits)])
+    df_vacina = pd.json_normalize(hits)
     if len(hits) != 0:
-      sleep(3)
+      # sleep(3)
       page_number += 1
-      for index, row in df_vacina.iterrows():
-        uf = row['_source.estabelecimento_uf']
-        df_vacina.loc[[index]].to_csv(f'datasets/{uf}.csv', mode='a', header=False)
+      # for index, row in df_vacina.iterrows():
+      #   uf = row['_source.estabelecimento_uf']
+      #   df_vacina.loc[[index]].to_csv(f'datasets/{uf}.csv', mode='a', header=False, index=False, index_label='_id')
     else:
       print('All pages fetched')
       break
   except Exception:
     traceback.print_exc()
     print("An error occurred, trying again in 3s")
-    sleep(3)
+    # sleep(3)
+    
+  end = time.time()
+  print(end - start, "seconds")
